3e035d7ae87c74811d20f2b806a8aefc2fb4c0b6
Gustavo Endringer Pereira <82174986+Theendringer@users.noreply.github.com>
2022-05-16 14:05:40 -0300

    é sobre isso, e est tudo bem (talvez)

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
 function myFunction(){
-
     location.href = "/atdandamento.html"
+}
+
+function iniciarAtendimento(){
+    location.href = "/iniciaratendimento.html"
+}
+
+function encerrarAtendimento(){
+    location.href = "/atendimento.html"
 }
