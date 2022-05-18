76633a7f30ee93c220fdfbe69a006f7c71fa2863
Gustavo Endringer Pereira <82174986+Theendringer@users.noreply.github.com>
2022-05-18 16:33:58 -0300

    funcao salvar

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function iniciarAtendimento(){
 function encerrarAtendimento(){
     location.href = "/atendimento.html"
 }
+
+function buscarSenha(){
+    location.href = "/configuracao.html"
+}
+
+
+function salvar() {
+            const bot = document.getElementById("campo-texto");
+            localStorage.setItem("senha", bot.value)
+            alert(localStorage.getItem("senha"))
+ }
