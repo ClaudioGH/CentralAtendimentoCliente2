d3b7422f2bbf728690061fa0c40199e5c875213e
Gustavo Endringer Pereira <82174986+Theendringer@users.noreply.github.com>
2022-05-23 17:03:22 -0300

    melhoria da experiencia do usuario

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,14 @@
 
 function myFunction(){
-    location.href = "/views/atdandamento.html"
+    location.href = "/atdandamento.html"
+}
+
+function chamarProx(){
+    location.href= "/resources/views/atdandamento.html"
 }
 
 function iniciarAtendimento(){
-    location.href = "/iniciaratendimento.html"
+    location.href = "views/iniciaratendimento.html"
 }
 
 function encerrarAtendimento(){
@@ -22,3 +26,92 @@ function salvar() {
             alert(localStorage.getItem("senha"))
 }
 
+function GetData() {
+
+    const bot = document.getElementById("data-atendimento");
+    //localStorage.setItem("senha", bot.value)
+    //alert(bot.value)
+
+    const uri = 'https://central-atendimento-cliente.herokuapp.com/api/atendimentos/dia/';
+            const initDetails = {
+                method: 'get',
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                },
+                mode: "cors"
+            }
+
+    fetch((uri +bot.value), initDetails )
+        .then( response =>
+        {
+            if ( response.status !== 200 )
+            {
+                console.log( 'Looks like there was a problem. Status Code: ' +
+                    response.status );
+                return;
+            }
+
+            console.log( response.headers.get( "Content-Type" ) );
+            return response.json();
+        }
+        ).then( myJson =>
+        {
+            console.log( JSON.stringify( myJson ) );
+            alert( JSON.stringify( myJson ) )
+            localStorage.setItem("requestResponse", JSON.stringify( myJson ));
+        } )
+        .catch( err =>
+        {
+            console.log( 'Fetch Error :-S', err );
+        } );
+}
+
+
+
+
+
+
+function GetFila(){
+
+    itemLista = document.getElementById("fila_espera"); 
+    
+    const dataAtendimento = document.getElementById("data-atendimento")
+    const uri = '/proxy.php';
+
+    itemLista.innerHTML = "";
+    const proxyParm = dataAtendimento.value;
+
+    console.log(proxyParm)
+
+    const loader = document.getElementById("progresso")
+
+    //console.log(loader)
+
+   const btnBuscar = document.getElementById("btn-buscar")  
+   btnBuscar.setAttribute("disabled", true)
+
+    loader.classList.add("progresso");
+
+    fetch(`${uri}?proxyParm=${proxyParm}`)
+
+       .then( r => r.json().then( r => {     
+        r.forEach(e => {
+
+             itemLista.innerHTML += `<li class="list-group-item">${e.numero_atendimento}${e.sufixo_atendimento}</li>`;
+        
+      
+             loader.classList.remove("progresso");
+
+             btnBuscar.removeAttribute("disabled");
+
+        });
+
+    })).catch(e => { 
+         alert("Ocorreu um erro ao tentar selecionar os atendimentos do dia.")
+         loader.classList.remove("progresso");
+         btnBuscar.removeAttribute("disabled");
+    })
+
+    
+
+}
---
+++ resources/proxy.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Proxy CORS
+ * @author Wanderlei Silva do Carmo <wander.silva@gmail.com>
+ * @version 1.0
+ * 
+ */
+
+
+ // create & initialize a curl session
+$curl = curl_init();
+$dataAtendimento = $_REQUEST["proxyParm"];
+
+$uri = "https://central-atendimento-cliente.herokuapp.com/api/atendimentos/dia/".$dataAtendimento;
+// set our url with curl_setopt()
+curl_setopt($curl, CURLOPT_URL, $uri);
+
+// return the transfer as a string, also with setopt()
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+// curl_exec() executes the started curl session
+// $output contains the output string
+$output = curl_exec($curl);
+
+print($output);
+
+// close curl resource to free up system resources
+// (deletes the variable made by curl_init)
+curl_close($curl);
