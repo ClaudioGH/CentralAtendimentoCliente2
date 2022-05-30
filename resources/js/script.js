806d12ab32ed1fdc30515e4f7a0ea98b3d913e50
caiodonatt@gmail.com <caiodonatt@gmail.com>
2022-05-30 02:15:32 -0300

    implementacao parcial da atualizacao da senha atual

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,3 +10,42 @@ function iniciarAtendimento(){
 function encerrarAtendimento(){
     location.href = "/atendimento.html"
 }
+function atualizarAatendimentoJS(){
+
+        const endPoint = 'https://central-atendimento-cliente.herokuapp.com/api';
+        const route = '/atendimentos/queue/next';
+
+
+        const initDetails = {
+            method: 'get',
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            mode: "cors"
+        }
+        
+        fetch((endPoint + route), initDetails )
+            .then( response =>
+            {
+                document.getElementById("senhaAtual").innerHTML = `${response.numero_atendimento}${response.sufixo_atendimento}`;
+
+                if ( response.status !== 200 )
+                {
+                    console.log( 'Looks like there was a problem. Status Code: ' +
+                        response.status );
+                    return;
+                }
+
+                console.log( response.headers.get( "Content-Type" ) );
+                return response.json();
+            }
+            ).then( myJson =>
+            {
+                document.getElementById("senhaAtual").innerHTML = `${myJson.numero_atendimento}-${myJson.sufixo_atendimento}`;
+                console.log( JSON.stringify( myJson ) );
+            } )
+            .catch( err =>
+            {
+                console.log( 'Fetch Error :-S', err );
+            } );
+}
