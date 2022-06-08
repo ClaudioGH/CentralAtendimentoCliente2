25684f55112aed0cd2d8977bcf4188f04e88a2af
Theendringer <>
2022-06-08 15:56:43 -0300

    refatoracao de guiche

---
+++ resources/js/triagem.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(evt){
+    document.getElementById("btn-buscar-triagem").addEventListener("click", function(){
+        const id = document.getElementById("buscar-triagem")
+        buscarId(id.value)
+    })
+})
+
+const buscarId = function(id){
+
+    triagem = document.getElementById("requerimento")
+    triagem.innerHTML = " "
+
+            
+    const uri = `https://central-atendimento-cliente.herokuapp.com/api/atendimento/id/${id}`
+    fetch(uri).then(r => r.json().then(r => {
+
+        requerimento.innerHTML += `<li class="list-group-item"> Senha:  ${r.numero_atendimento}${r.sufixo_atendimento}</li>`
+        requerimento.innerHTML += `<li class="list-group-item"> CPF:  ${r.cpf}</li>`
+        requerimento.innerHTML += `<li class="list-group-item"> Data e Hora da Emissao:  ${r.date_time_emissao_atendimento}</li>`
+        requerimento.innerHTML += `<li class="list-group-item"> Estado:  ${r.inicio_atendimento}</li>`
+        requerimento.innerHTML += `<li class="list-group-item"> Atendimento:  ${r.fim_atendimento}</li>`
+        requerimento.innerHTML += `<li class="list-group-item"> ID Atendimento:  ${r.id_atendimento}</li>`
+        requerimento.innerHTML += `<li class="list-group-item"> Observações:  ${r.observacoes}</li>`
+
+    }))
+
+}
